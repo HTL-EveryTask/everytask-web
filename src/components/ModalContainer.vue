26892067835ef5914fcd050082bf27557396f129
Nekonyx13 <corvin.prath@gmail.com>
2022-10-26 14:38:58 +0200

    added modal animation

--- src/components/ModalContainer.vue
+++ src/components/ModalContainer.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
-defineEmits(["close"]);
+import { onMounted } from "vue";
+
+const emit = defineEmits(["close"]);
 
 defineProps({
   title: {
@@ -14,32 +16,76 @@ defineProps({
       return ["none", "shadow"].includes(value);
     },
   },
+
+  show: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+onMounted(() => {
+  // add event listener to close modal on escape key press
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      emit("close");
+    }
+  });
 });
 </script>
 
 <template>
-  <div
-    :class="{
-      'overlay-shadow': effect === 'shadow',
-    }"
-    class="absolute w-screen h-screen left-0 top-0"
-    @click="$emit('close')"
-  />
-  <div
-    class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 bg-white rounded-lg shadow-lg z-10"
-  >
-    <div class="flex justify-between items-center p-4">
-      <h1 class="text-xl font-bold">{{ title }}</h1>
-      <button class="text-2xl" @click="$emit('close')">×</button>
+  <Transition name="overlay">
+    <div
+      v-if="show"
+      :class="{
+        'overlay-shadow': effect === 'shadow',
+      }"
+      class="absolute w-screen h-screen left-0 top-0"
+      @click="$emit('close')"
+    />
+  </Transition>
+
+  <Transition appear name="modal">
+    <div
+      v-if="show"
+      class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 bg-white rounded-lg shadow-lg z-10"
+    >
+      <div class="flex justify-between items-center p-4">
+        <h1 class="text-xl font-bold">{{ title }}</h1>
+        <button class="text-2xl" @click="$emit('close')">×</button>
+      </div>
+      <div class="p-4">
+        <slot></slot>
+      </div>
     </div>
-    <div class="p-4">
-      <slot></slot>
-    </div>
-  </div>
+  </Transition>
 </template>
 
+<!--suppress CssUnusedSymbol -->
 <style scoped>
 .overlay-shadow {
-  @apply bg-slate-800 opacity-20;
+  @apply bg-slate-800 bg-opacity-20 backdrop-blur-sm;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+  @apply scale-50;
+}
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: transform 0.4s cubic-bezier(0.25, 0.25, 0, 1.21),
+    opacity 0.2s ease;
+}
+
+.overlay-enter-from,
+.overlay-leave-to {
+  opacity: 0;
+}
+
+.overlay-enter-active,
+.overlay-leave-active {
+  transition: opacity 0.2s ease;
 }
 </style>
