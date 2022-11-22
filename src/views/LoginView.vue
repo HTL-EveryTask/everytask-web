0f405c52e52670ab464dbe683da10ffb27e5abcb
Nekonyx13 <corvin.prath@gmail.com>
2022-11-22 15:17:23 +0100

    fixed scrollable login and register

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -53,11 +53,10 @@ async function onSubmit() {
 </script>
 
 <template>
-  <div class="w-full h-full">
+  <div class="w-full h-full overflow-y-hidden">
     <div
       class="p-8 sm:p-4 mx-auto rounded-3xl sm:rounded-none flex flex-col shadow-lg shadow-yonder/10 max-w-[36em] sm:w-full sm:h-full bg-white mt-16"
     >
-      <section></section>
       <div class="mb-6 mt-2">
         <img
           alt="logo"
